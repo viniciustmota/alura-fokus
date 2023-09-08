8fd0bf6c862e78b0b181ba8b8acad7038ef49990
unknown <motaviny140@gmail.com>
2023-09-08 17:33:37 -0300

    Função cronometro parcialmente finalizado

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const longoBtn = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBtn = document.querySelector("#start-pause");
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+musica.loop = true;
+
+const musicaIniciar = new Audio("/sons/play.wav");
+const musicaPausar = new Audio("/sons/pause.mp3");
+const musicaAlarme = new Audio("/sons/beep.mp3");
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 focoBtn.addEventListener("click", () => {
   alterarContexto("foco");
@@ -44,4 +63,32 @@ function alterarContexto(contexto) {
     default:
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    zerar()
+    musicaAlarme.play()
+    alert("Tempo finalizado");
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log(`Temporizador: ${tempoDecorridoEmSegundos}`);
+};
+
+startPauseBtn.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        zerar()
+        musicaPausar.play()
+        return
+    }
+    musicaIniciar.play()
+  intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
