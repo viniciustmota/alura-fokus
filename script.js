dd54ffdf83e92c74c3320ae1c27289c8f842abf5
unknown <motaviny140@gmail.com>
2023-09-08 15:06:00 -0300

    Função alterar contexto criada

---
+++ script.js
@@ -0,0 +1,47 @@
+const html = document.querySelector("html");
+const focoBtn = document.querySelector(".app__card-button--foco");
+const curtoBtn = document.querySelector(".app__card-button--curto");
+const longoBtn = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+
+focoBtn.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBtn.classList.add("active");
+});
+
+curtoBtn.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBtn.classList.add("active");
+});
+
+longoBtn.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBtn.classList.add("active");
+});
+
+function alterarContexto(contexto) {
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada?<br>
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+    default:
+      break;
+  }
+}
